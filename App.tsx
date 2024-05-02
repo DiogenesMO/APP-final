0095e2b19efcc768ffa68384116aa083c3f80ccc
DiogenesMO <orozcomojica29@gmail.com>
2024-05-01 21:15:42 -0600

    Cambiando la dirección de la imagen

--- App.tsx
+++ App.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import ProfileCard from './src/components/ProfileCard';
 
-
-
 const App: React.FC = () => {
   return (
     <View style={styles.container}>
       <ProfileCard
-        profilePicture="https://th.bing.com/th/id/OIP.6SBJtG_5zsxnpXrAKp5rGgHaJU?w=156&h=197&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+        profilePicture="https://social-links-profile-main-henna.vercel.app/_next/image?url=%2Fassets%2Fimages%2Favatar-jessica.jpeg&w=3840&q=75"
         userName="Jessica Randall"
         address="London, United kingdom"
         description="Front-end developer and avid reader."
